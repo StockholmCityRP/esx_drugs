c018ab7b2c8316d5371dd69a6ed0a7e57ae58ade
ElPumpo <samo_kol@hotmail.com>
2018-02-03 16:55:16 +0100

    Initial push

--- config.lua
+++ config.lua
@@ -2,12 +2,12 @@ Config              = {}
 Config.MarkerType   = 1
 Config.DrawDistance = 100.0
 Config.ZoneSize     = {x = 5.0, y = 5.0, z = 3.0}
---Config.MarkerColor  = {r = 100, g = 204, b = 100}
-Config.RequiredCopsCoke = 2
-Config.RequiredCopsMeth = 2
+Config.MarkerColor  = {r = 100, g = 204, b = 100}
+Config.RequiredCopsCoke = 0
+Config.RequiredCopsMeth = 0
 Config.RequiredCopsWeed = 0
-Config.RequiredCopsOpium = 3
-Config.Locale = 'fr'
+Config.RequiredCopsOpium = 0
+Config.Locale = 'en'
 
 Config.Zones = {
 	CokeFarm = 		 {x=2448.9228515625,  y=-1836.8076171875, z=51.953701019287},
--- esx_drugs.sql
+++ esx_drugs.sql
@@ -1,10 +1,12 @@
+USE `fivem2337`;
+
 INSERT INTO `items` (name, label) VALUES
 	('weed', 'Weed'),
-	('weed_pooch', 'Pochon de weed'),
+	('weed_pooch', 'Pouch of weed'),
 	('coke', 'Coke'),
-	('coke_pooch', 'Pochon de coke'),
+	('coke_pooch', 'Pouch of coke'),
 	('meth', 'Meth'),
-	('meth_pooch', 'Pochon de meth'),
+	('meth_pooch', 'Pouch of meth'),
 	('opium', 'Opium'),
-	('opium_pooch', 'Pochon de opium')
+	('opium_pooch', 'Pouch of opium')
 ;

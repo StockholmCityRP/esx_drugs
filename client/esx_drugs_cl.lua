9f5e3deaf2bda9911b9860ab6a16471aba76a9cb
ElPumpo <samo_kol@hotmail.com>
2018-02-08 21:54:35 +0100

    Added blips

--- client/esx_drugs_cl.lua
+++ client/esx_drugs_cl.lua
@@ -319,3 +319,18 @@ Citizen.CreateThread(function()
         end
     end
 end)
+
+-- blips
+Citizen.CreateThread(function()
+	for _, info in pairs(Config.blips) do
+		info.blip = AddBlipForCoord(info.x, info.y, info.z)
+		SetBlipSprite(info.blip, info.sprite)
+		SetBlipDisplay(info.blip, 4)
+		SetBlipScale(info.blip, 1.5)
+		SetBlipColour(info.blip, info.color)
+		SetBlipAsShortRange(info.blip, true)
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString(info.title)
+		EndTextCommandSetBlipName(info.blip)
+	end
+end)

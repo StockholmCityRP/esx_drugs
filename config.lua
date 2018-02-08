b0e32c6d16267252a2808ce8f48633750a0e1f55
ElPumpo <samo_kol@hotmail.com>
2018-02-08 22:02:39 +0100

    Updated blips

--- config.lua
+++ config.lua
@@ -26,16 +26,16 @@ Config.Zones = {
 
 --https://wiki.gtanet.work/index.php?title=Blips
 Config.blips = {
-	{title="Cocaine Field", color=4, sprite=501, x=2448.9228515625, y=-1836.8076171875, z=51.953701019287},
-	{title="Cocaine Processing", color=4, sprite=478, x=-458.13967895508, y=-2278.6174316406, z=7.5158290863037},
+	{title="Cocaine Field", color=40, sprite=501, x=2448.9228515625, y=-1836.8076171875, z=51.953701019287},
+	{title="Cocaine Processing", color=40, sprite=478, x=-458.13967895508, y=-2278.6174316406, z=7.5158290863037},
 	{title="Cocaine Drug Dealer", color=75, sprite=500, x=-1756.1984863281, y=427.31674194336,  z=126.68292999268},
 	{title="Meth Field", color=26, sprite=499, x=1525.298828125, y=1710.0217285156, z=109.00956726074},
 	{title="Meth Processing", color=26, sprite=499, x=-1001.4151611328, y=4848.0087890625,  z=274.00686645508},
 	{title="Meth Drug Dealer", color=75, sprite=500, x=-63.592178344727, y=-1224.0709228516, z=27.768648147583},
-	{title="Weed Field", color=25, sprite=496, x=1609.125, y=6663.5942382813, z=20.961572647095},
-	{title="Weed Processing", color=38, sprite=496, x=91.061386108398,  y=3750.0380859375,  z=39.77326965332},
+	{title="Weed Field", color=52, sprite=496, x=1609.125, y=6663.5942382813, z=20.961572647095},
+	{title="Weed Processing", color=52, sprite=496, x=91.061386108398,  y=3750.0380859375,  z=39.77326965332},
 	{title="Weed Drug Dealer", color=75, sprite=500, x=-54.249694824219, y=-1443.3666992188, z=31.068626403809},
 	{title="Opium Field", color=60, sprite=121, x=1972.784790039,	 y=3819.3999023438,  z=33.428722381592},
-	{title="Opium Processing", color=60, sprite=378, x=971.86499023438,y=-2157.61328125, z=28.475107192994},
+	{title="Opium Processing", color=60, sprite=51, x=971.86499023438,y=-2157.61328125, z=28.475107192994},
 	{title="Opium Drug Dealer", color=75, sprite=500, x=2331.0881347656,y=2570.2250976562,z=46.681819915772}
 }
